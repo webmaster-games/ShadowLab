d35bb3512c5897b485ee91a2e8919fb7feb96ae5
Navlis93 <suvorov.alex93@gmail.com>
2016-01-24 20:33:15 +0300

    some changes

--- js/app.js
+++ js/app.js
@@ -30,21 +30,64 @@ function main() {
     requestAnimFrame(main);
 };
 function init() {
-    terrainPattern = ctx.createPattern(resources.get('img/terrain-one.png'), 'repeat');
+    //terrainPattern = ctx.createPattern(resources.get('img/terrain-one.png'), 'repeat');
+    generateMap();
     lastTime = Date.now();
     main();
+}
+function generateMap() {
+    var i;
+    var j;
+    for (i = 0; i < map.length; i++){
+        for (j = 0; j < map[i].length; j++){
+            if(map[i][j] == 1){
+                walls.push({
+                    pos: [i*64,j*64],
+                    size: [64,64],
+                    sprite: new Sprite('img/wall-sprite.png', [0,0],[64,64],0),
+                });
+            }else{
+                spaces.push({
+                    pos: [i*64,j*64],
+                    size: [64,64],
+                    sprite: new Sprite('img/space-sprite.png', [0,0],[64,64],0),
+                });
+            }
+        }
+    }
 }
 
 resources.load([
     'img/player-sprite.png',
-    'img/terrain-one.png'
+    'img/space-sprite.png',
+    'img/wall-sprite.png',
+    'img/terrain-one.png',
 ]);
 resources.onReady(init);
-
+var map = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1],
+    [1,0,1,1,1,1,1,1,1,0,0,1,1,0,1,1],
+    [1,0,0,0,0,0,1,1,1,0,1,1,1,0,1,1],
+    [1,1,1,0,0,0,1,1,1,0,0,1,1,0,1,1],
+    [1,1,1,0,0,0,1,1,0,0,0,1,1,0,1,1],
+    [1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1],
+    [1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1],
+    [1,1,0,0,0,1,1,0,0,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,0,1,1,1,1,1,0,1,1],
+    [1,1,0,0,0,1,1,0,0,0,0,0,1,0,1,1],
+    [1,1,1,1,0,1,1,1,1,0,0,0,1,0,1,1],
+    [1,1,1,1,0,1,1,1,1,1,0,0,1,1,1,1],
+    [1,1,1,1,0,0,1,1,1,1,1,0,1,1,1,1],
+    [1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
+var walls = [];
+var spaces = [];
 var player = {
-    pos: [canvas.width/2, canvas.height/2],
+    pos: [64, 64],
     size: [64,64],
-    sprite: new Sprite('img/player-sprite.png', [0, 0], [64, 64], 4, [0, 1, 2, 3]),
+    sprite: new Sprite('img/player-sprite.png', [0, 0], [64, 64], 8, [0, 1, 2, 3]),
     speed: 100,
     visibleRadius: 100,
 };
@@ -57,29 +100,54 @@ function update(dt){
 }
 function handleInput(dt) {
 	if (input.isNothingToDo()){
-		player.sprite.pos = [0,0];
+		//player.sprite.pos = [0,0];
     	player.sprite.reset();
 	}else{
     	if(input.isDown('DOWN') || input.isDown('s')) {
     	    player.pos[1] += player.speed * dt;
+            for(var i = 0; i < walls.length; i++) {
+                if(isCollide(walls[i],player)){
+                    player.pos[1] = walls[i].pos[1]-player.size[1];
+                    break;
+                }
+            }
+            //ctx.translate(0,-player.speed * dt);
     	    player.sprite.pos = [0,0];
     	    player.sprite.update(dt);
     	}
 
     	if(input.isDown('UP') || input.isDown('w')) {
     	    player.pos[1] -= player.speed * dt;
+            for(var i = 0; i < walls.length; i++) {
+                if(isCollide(walls[i],player)){
+                    player.pos[1] = walls[i].pos[1]+walls[i].size[1];break;
+                }
+            }
+            //ctx.translate(0,player.speed * dt);
     	    player.sprite.pos = [0,196];
     	    player.sprite.update(dt);
     	}
 
     	if(input.isDown('LEFT') || input.isDown('a')) {
     	    player.pos[0] -= player.speed * dt;
+            for(var i = 0; i < walls.length; i++) {
+                if(isCollide(walls[i],player)){
+                    player.pos[0] = walls[i].pos[0]+walls[i].size[0];break;
+                }
+            }
+            //ctx.translate(player.speed * dt, 0);
     	    player.sprite.pos = [0,64];
     	    player.sprite.update(dt);
     	}
 
     	if(input.isDown('RIGHT') || input.isDown('d')) {
     	    player.pos[0] += player.speed * dt;
+            for(var i = 0; i < walls.length; i++) {
+                if(isCollide(walls[i],player)){
+                    player.pos[0] = walls[i].pos[0]-player.size[0];break;
+                }
+            }
+            //ctx.translate(-player.speed * dt,0);
     	    player.sprite.pos = [0,128];
     	    player.sprite.update(dt);
     	}
@@ -91,18 +159,54 @@ function updateEntities(dt) {
 }
 
 function render() {
-	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
 
-	ctx.fillStyle = terrainPattern;
-    ctx.fillRect(player.pos[0] - player.visibleRadius, player.pos[1] - player.visibleRadius, player.visibleRadius*2, player.visibleRadius*2);
-
+    renderMap();
+	
+    /*
+	ctx.fillRect(0, 0, player.pos[0] + player.size[0]/2 - player.visibleRadius, canvas.height);
+    ctx.fillRect(player.pos[0] + player.size[0]/2 - player.visibleRadius, 0, canvas.width, player.pos[1] + player.size[1]/2 - player.visibleRadius);
+    ctx.fillRect(player.pos[0] + player.size[0]/2 + player.visibleRadius, 0, canvas.width, player.visibleRadius*2);
+    ctx.fillRect(player.pos[0] + player.size[0]/2 - player.visibleRadius, player.pos[1] + player.size[1]/2 + player.visibleRadius, canvas.width, canvas.height);
+    */
     renderEntity(player);
 }
+function renderMap() {
+    for(var i = 0; i < walls.length; i++) {
+        if(isVisible(walls[i].pos,walls[i].size)){
+            renderEntity(walls[i]);
+        }
+    }
+    for(i = 0; i < spaces.length; i++) {
+        if(isVisible(spaces[i].pos,spaces[i].size)){
+            renderEntity(spaces[i]);
+        }
+    }
+}
 
+function isVisible(pos,size) {
+    var playerX = player.pos[0]+player.size[0]/2;
+    var playerY = player.pos[1]+player.size[1]/2;
+
+    if(playerX-player.visibleRadius <= pos[0]+size[0]/2 && playerX+player.visibleRadius >= pos[0]+size[0]/2)
+        if(playerY-player.visibleRadius <= pos[1]+size[1]/2 && playerY+player.visibleRadius >= pos[1]+size[1]/2)
+            return true;
+    return false;
+}
+function isCollide(entity, entity2){
+    var entityX = entity.pos[0]+entity.size[0]/2;
+    var entity2X = entity2.pos[0]+entity2.size[0]/2;
+    var entityY = entity.pos[1]+entity.size[1]/2;
+    var entity2Y = entity2.pos[1]+entity2.size[1]/2;
+    if(Math.abs(entityX - entity2X) < (entity.size[0] + entity2.size[0])/2)
+       if(Math.abs(entityY - entity2Y) < (entity.size[1] + entity2.size[1])/2) 
+        return true;
+    return false;
+}
 function renderEntity(entity) {
     ctx.save();
-    ctx.translate(entity.pos[0]-entity.size[0]/2, entity.pos[1]-entity.size[1]/2);
+    ctx.translate( entity.pos[0], entity.pos[1] + entity.size[1] - entity.sprite.size[1]);
     entity.sprite.render(ctx);
     ctx.restore();
 }
